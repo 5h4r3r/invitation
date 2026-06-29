43a4571876b3aa97b1dcc73515a7d97cd0068527
Sharer <sharer@example.com>
2026-06-29 20:28:03 +0500

    fix: вернул старый рабочий URL API, POST для формы оставил

--- src/composables/useGuest.js
+++ src/composables/useGuest.js
@@ -1,6 +1,6 @@
 import { ref } from 'vue'
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbwRB8fdz281R-TstyN5DIjx5DHxwOI_hv-Bg_5IIw61cU2Ss-_vpKcVXjnYPI5jKikNGw/exec'
+const API_URL = 'https://script.google.com/macros/s/AKfycbx1iguaUjx-HedGgVuYdSfvxlRQyiRom0fyoWf_zgYKycFLdVuznOpxEUvIJkK0XCV1/exec'
 
 const guestName = ref('')
 const confirmed = ref(false)
