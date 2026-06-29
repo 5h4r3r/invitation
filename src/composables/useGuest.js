5b00983ccc5b9ca72909ff65957faf25a6eb4b19
Sharer <sharer@example.com>
2026-06-29 20:24:09 +0500

    deploy: новый URL API #2

--- src/composables/useGuest.js
+++ src/composables/useGuest.js
@@ -1,6 +1,6 @@
 import { ref } from 'vue'
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbyKu61aJcXFXp1UIX2yjiItefFPcQS01Op49E8auFDghEi-dVolQkE7On8NwJFGKqaegw/exec'
+const API_URL = 'https://script.google.com/macros/s/AKfycbwRB8fdz281R-TstyN5DIjx5DHxwOI_hv-Bg_5IIw61cU2Ss-_vpKcVXjnYPI5jKikNGw/exec'
 
 const guestName = ref('')
 const confirmed = ref(false)
